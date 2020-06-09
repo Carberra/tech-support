93597a9b76012e5eacda2155528d2aa8338abe38
Max Shaughnessy <msh@lap.is>
2020-06-09 01:47:59 +0100

    Add `terminate` command for staff

--- techsupport/bot/cogs/staff.py
+++ techsupport/bot/cogs/staff.py
@@ -11,6 +11,11 @@ class Staff(commands.Cog):
     def cog_check(self, ctx):
         return commands.has_role("Staff")
 
+    @commands.command()
+    async def terminate(self, ctx, reason):
+        ctx.command = self.bot.get_command("resolve")
+        await ctx.invoke(ctx.command, "terminate", reason=reason)
+
 
 def setup(bot):
     bot.add_cog(Staff(bot))
